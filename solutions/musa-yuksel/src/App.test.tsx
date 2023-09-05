d76fb540ee87c50a981ddcaa0d10ed0787852f4d
Musa <79163385+musayuksel@users.noreply.github.com>
2023-09-05 20:35:52 +0100

    Add app test and code for initial values

--- solutions/musa-yuksel/src/App.test.tsx
+++ solutions/musa-yuksel/src/App.test.tsx
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { IPlayer } from './interfaces/IPlayer';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+jest.mock('react', () => ({
+  ...jest.requireActual('react'),
+  useState: jest.fn(),
+}));
+
+const mockPlayer1: IPlayer = {
+  name: 'Mock Player 1',
+  score: 0,
+};
+const mockPlayer2: IPlayer = {
+  name: 'Mock Player 2',
+  score: 0,
+};
+
+describe('App', () => {
+  it('should call the Scoreboard with players', () => {
+    (useState as jest.Mock).mockReturnValueOnce([
+      [mockPlayer1, mockPlayer2],
+      jest.fn(),
+    ]);
+
+    render(<App />);
+
+    expect(screen.getByText(mockPlayer1.name)).toBeInTheDocument();
+  });
 });
--- solutions/musa-yuksel/src/App.tsx
+++ solutions/musa-yuksel/src/App.tsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './App.module.css';
+import { IPlayer } from './interfaces/IPlayer';
+import { Scoreboard } from './components';
 
 function App() {
+  const player1: IPlayer = {
+    name: 'Player 1',
+    score: 0,
+  };
+  const player2: IPlayer = {
+    name: 'Player 2',
+    score: 0,
+  };
+  const [players, setPlayers] = useState([player1, player2]);
   return (
     <div className={styles.app}>
-      <h1>Musa Yuksel</h1>
+      <Scoreboard {...players[0]} />
+      <Scoreboard {...players[1]} />
     </div>
   );
 }
