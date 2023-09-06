6c9937f8d63edddd1b783006374f5bb0bc8c1be1
Musa <79163385+musayuksel@users.noreply.github.com>
2023-09-06 21:36:55 +0100

    Add deuce adv and winner

--- solutions/musa-yuksel/src/App.tsx
+++ solutions/musa-yuksel/src/App.tsx
@@ -14,18 +14,55 @@ function App() {
   };
   const [players, setPlayers] = React.useState([player1, player2]);
 
+  const isDeuce =
+    players[0].score >= 3 && players[0].score === players[1].score;
+
+  const [hasPlayerWon, setHasPlayerWon] = React.useState({
+    player1: false,
+    player2: false,
+  });
+
+  const [hasPlayerAdvantage, setHasPlayerAdvantage] = React.useState({
+    player1: false,
+    player2: false,
+  });
+
+  React.useEffect(() => {
+    if (players[0].score >= 4 && players[0].score - players[1].score === 1) {
+      setHasPlayerAdvantage((prev) => ({ player2: false, player1: true }));
+    }
+    if (players[1].score >= 4 && players[1].score - players[0].score === 1) {
+      setHasPlayerAdvantage((prev) => ({ player1: false, player2: true }));
+    }
+  }, [players]);
+
+  React.useEffect(() => {
+    if (players[0].score >= 4 && players[0].score - players[1].score >= 2) {
+      setHasPlayerWon((prev) => ({ ...prev, player1: true }));
+    }
+    if (players[1].score >= 4 && players[1].score - players[0].score >= 2) {
+      setHasPlayerWon((prev) => ({ ...prev, player2: true }));
+    }
+  }, [players]);
+
   const increasePlayer1Score = () => {
     setPlayers((prevPlayers) => {
       const [player1, player2] = prevPlayers;
-      return [{ ...player1, score: player1.score++ }, player2];
+      return [{ ...player1, score: player1.score + 1 }, player2];
     });
   };
 
   const increasePlayer2Score = () => {
     setPlayers((prevPlayers) => {
       const [player1, player2] = prevPlayers;
-      return [player1, { ...player2, score: player2.score++ }];
+      return [player1, { ...player2, score: player2.score + 1 }];
     });
+  };
+
+  const resetTheMatch = () => {
+    setPlayers([player1, player2]);
+    setHasPlayerWon({ player1: false, player2: false });
+    setHasPlayerAdvantage({ player1: false, player2: false });
   };
 
   return (
@@ -35,12 +72,28 @@ function App() {
         <li>Game Score</li>
       </ul>
 
-      <Scoreboard {...players[0]} />
-      <Scoreboard {...players[1]} />
+      <Scoreboard
+        {...players[0]}
+        isDeuce={isDeuce}
+        hasAdvantage={hasPlayerAdvantage.player1}
+        hasWon={hasPlayerWon.player1}
+      />
+      <Scoreboard
+        {...players[1]}
+        isDeuce={isDeuce}
+        hasAdvantage={hasPlayerAdvantage.player2}
+        hasWon={hasPlayerWon.player2}
+      />
 
       <div>
-        <button onClick={increasePlayer1Score}>Player1 +</button>
-        <button onClick={increasePlayer2Score}>Player2 +</button>
+        {hasPlayerWon.player1 || hasPlayerWon.player2 ? (
+          <button onClick={resetTheMatch}>NEW MATCH </button>
+        ) : (
+          <>
+            <button onClick={increasePlayer1Score}>Player1 +</button>
+            <button onClick={increasePlayer2Score}>Player2 +</button>
+          </>
+        )}
       </div>
     </section>
   );
--- solutions/musa-yuksel/src/components/scoreboard/Scoreboard.tsx
+++ solutions/musa-yuksel/src/components/scoreboard/Scoreboard.tsx
@@ -2,14 +2,34 @@ import React, { type FC } from 'react';
 import { IPlayer } from '../../interfaces/IPlayer';
 import styles from './Scoreboard.module.css';
 
-export const Scoreboard: FC<IPlayer> = ({ name, score }) => {
+interface ScoreboardProps extends IPlayer {
+  isDeuce: boolean;
+  hasWon: boolean;
+  hasAdvantage: boolean;
+}
+
+export const Scoreboard: FC<ScoreboardProps> = ({
+  name,
+  score,
+  isDeuce,
+  hasAdvantage,
+  hasWon,
+}) => {
   const boardScore =
-    score === 0 ? '0' : score === 1 ? '15' : score === 2 ? '30' : '40';
+    score === 0 ? 'LOVE' : score === 1 ? '15' : score === 2 ? '30' : '40';
 
   return (
     <ul className={styles.scoreboardContainer}>
       <li>{name}</li>
-      <li>{boardScore}</li>
+      <li>{`${
+        hasWon
+          ? 'WINNER'
+          : isDeuce
+          ? 'DEUCE'
+          : hasAdvantage
+          ? 'ADV'
+          : boardScore
+      }`}</li>
     </ul>
   );
 };
