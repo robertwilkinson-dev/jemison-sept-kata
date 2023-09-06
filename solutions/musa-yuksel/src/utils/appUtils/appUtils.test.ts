b23e9017c5e3622fa652ef12e4ae911c156318eb
Musa <79163385+musayuksel@users.noreply.github.com>
2023-09-06 23:20:01 +0100

    Add hasPlayer2Advantage test

--- solutions/musa-yuksel/src/utils/appUtils/appUtils.test.ts
+++ solutions/musa-yuksel/src/utils/appUtils/appUtils.test.ts
@@ -1,4 +1,4 @@
-import { hasPlayer1Advantage } from './appUtils';
+import { hasPlayer1Advantage, hasPlayer2Advantage } from './appUtils';
 
 describe('hasPlayer1Advantage', () => {
   it('should return true when player 1 has advantage', () => {
@@ -25,6 +25,35 @@ describe('hasPlayer1Advantage', () => {
       { name: 'Player 2', score: 6 },
     ];
     const result = hasPlayer1Advantage(players);
+    expect(result).toBe(false);
+  });
+});
+
+describe('hasPlayer2Advantage', () => {
+  it('should return true when player 2 has advantage', () => {
+    const players = [
+      { name: 'Player 1', score: 4 },
+      { name: 'Player 2', score: 5 },
+    ];
+    const result = hasPlayer2Advantage(players);
+    expect(result).toBe(true);
+  });
+
+  it('should return false when score is lover than 4', () => {
+    const players = [
+      { name: 'Player 1', score: 3 },
+      { name: 'Player 2', score: 3 },
+    ];
+    const result = hasPlayer2Advantage(players);
+    expect(result).toBe(false);
+  });
+
+  it('returns false when player 1 has advantage', () => {
+    const players = [
+      { name: 'Player 1', score: 6 },
+      { name: 'Player 2', score: 5 },
+    ];
+    const result = hasPlayer2Advantage(players);
     expect(result).toBe(false);
   });
 });
