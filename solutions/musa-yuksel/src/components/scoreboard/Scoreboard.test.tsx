812e8d504f03b79f6f6fa86db828e98c6fa9ad64
Musa <79163385+musayuksel@users.noreply.github.com>
2023-09-07 15:03:22 +0100

    Add more test for Scoreboard

--- solutions/musa-yuksel/src/components/scoreboard/Scoreboard.test.tsx
+++ solutions/musa-yuksel/src/components/scoreboard/Scoreboard.test.tsx
@@ -1,7 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import { Scoreboard } from './Scoreboard';
 import { IPlayer } from '../../interfaces';
-// import { IPlayer } from '../../interfaces/IPlayer';
 
 describe('Scoreboard', () => {
   const player: IPlayer = {
@@ -33,5 +32,85 @@ describe('Scoreboard', () => {
     );
 
     expect(screen.getByText('LOVE')).toBeInTheDocument();
+  });
+
+  it('should show score as 15. Score:1', () => {
+    render(
+      <Scoreboard
+        {...player}
+        score={1}
+        isDeuce={false}
+        hasWon={false}
+        hasAdvantage={false}
+      />
+    );
+
+    expect(screen.getByText('15')).toBeInTheDocument();
+  });
+
+  it('should show score as 30. Score:2', () => {
+    render(
+      <Scoreboard
+        {...player}
+        score={2}
+        isDeuce={false}
+        hasWon={false}
+        hasAdvantage={false}
+      />
+    );
+
+    expect(screen.getByText('30')).toBeInTheDocument();
+  });
+
+  it('should show score as 40. Score:3', () => {
+    render(
+      <Scoreboard
+        {...player}
+        score={3}
+        isDeuce={false}
+        hasWon={false}
+        hasAdvantage={false}
+      />
+    );
+
+    expect(screen.getByText('40')).toBeInTheDocument();
+  });
+
+  it('should show score as WINNER. hasWon=true', () => {
+    render(
+      <Scoreboard
+        {...player}
+        score={4}
+        isDeuce={false}
+        hasWon={true}
+        hasAdvantage={false}
+      />
+    );
+
+    expect(screen.getByText('WINNER')).toBeInTheDocument();
+  });
+
+  it('should show score as DEUCE. isDeuce=true', () => {
+    render(
+      <Scoreboard
+        {...player}
+        isDeuce={true}
+        hasWon={false}
+        hasAdvantage={false}
+      />
+    );
+  });
+
+  it('should show score as ADVANTAGE. hasAdvantage=true', () => {
+    render(
+      <Scoreboard
+        {...player}
+        isDeuce={false}
+        hasWon={false}
+        hasAdvantage={true}
+      />
+    );
+
+    expect(screen.getByText('ADVANTAGE')).toBeInTheDocument();
   });
 });
