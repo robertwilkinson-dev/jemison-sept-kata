987585adaaf9281b5caa5bcc485b080a5c8b5c80
Musa <79163385+musayuksel@users.noreply.github.com>
2023-09-07 15:53:17 +0100

    Refactor

--- solutions/musa-yuksel/src/components/scoreboard/Scoreboard.test.tsx
+++ solutions/musa-yuksel/src/components/scoreboard/Scoreboard.test.tsx
@@ -1,115 +1,58 @@
 import { render, screen } from '@testing-library/react';
 import { Scoreboard } from './Scoreboard';
-import { IPlayer } from '../../interfaces';
+import { IPlayer, IScoreboard } from '../../interfaces';
 
 describe('Scoreboard', () => {
-  const player: IPlayer = {
+  const mockScoreboardProps: IScoreboard = {
     name: 'Musa',
     score: 0,
+    isDeuce: false,
+    hasAdvantage: false,
+    hasWon: false,
   };
 
   it('should show the player name', () => {
-    render(
-      <Scoreboard
-        {...player}
-        isDeuce={false}
-        hasWon={false}
-        hasAdvantage={false}
-      />
-    );
+    render(<Scoreboard {...mockScoreboardProps} />);
 
-    expect(screen.getByText(player.name)).toBeInTheDocument();
+    expect(screen.getByText(mockScoreboardProps.name)).toBeInTheDocument();
   });
 
   it('should show score as LOVE. Score:0', () => {
-    render(
-      <Scoreboard
-        {...player}
-        isDeuce={false}
-        hasWon={false}
-        hasAdvantage={false}
-      />
-    );
+    render(<Scoreboard {...mockScoreboardProps} />);
 
     expect(screen.getByText('LOVE')).toBeInTheDocument();
   });
 
   it('should show score as 15. Score:1', () => {
-    render(
-      <Scoreboard
-        {...player}
-        score={1}
-        isDeuce={false}
-        hasWon={false}
-        hasAdvantage={false}
-      />
-    );
+    render(<Scoreboard {...mockScoreboardProps} score={1} />);
 
     expect(screen.getByText('15')).toBeInTheDocument();
   });
 
   it('should show score as 30. Score:2', () => {
-    render(
-      <Scoreboard
-        {...player}
-        score={2}
-        isDeuce={false}
-        hasWon={false}
-        hasAdvantage={false}
-      />
-    );
+    render(<Scoreboard {...mockScoreboardProps} score={2} />);
 
     expect(screen.getByText('30')).toBeInTheDocument();
   });
 
   it('should show score as 40. Score:3', () => {
-    render(
-      <Scoreboard
-        {...player}
-        score={3}
-        isDeuce={false}
-        hasWon={false}
-        hasAdvantage={false}
-      />
-    );
+    render(<Scoreboard {...mockScoreboardProps} score={3} />);
 
     expect(screen.getByText('40')).toBeInTheDocument();
   });
 
   it('should show score as WINNER. hasWon=true', () => {
-    render(
-      <Scoreboard
-        {...player}
-        score={4}
-        isDeuce={false}
-        hasWon={true}
-        hasAdvantage={false}
-      />
-    );
+    render(<Scoreboard {...mockScoreboardProps} hasWon={true} />);
 
     expect(screen.getByText('WINNER')).toBeInTheDocument();
   });
 
   it('should show score as DEUCE. isDeuce=true', () => {
-    render(
-      <Scoreboard
-        {...player}
-        isDeuce={true}
-        hasWon={false}
-        hasAdvantage={false}
-      />
-    );
+    render(<Scoreboard {...mockScoreboardProps} isDeuce={true} />);
   });
 
   it('should show score as ADVANTAGE. hasAdvantage=true', () => {
-    render(
-      <Scoreboard
-        {...player}
-        isDeuce={false}
-        hasWon={false}
-        hasAdvantage={true}
-      />
-    );
+    render(<Scoreboard {...mockScoreboardProps} hasAdvantage={true} />);
 
     expect(screen.getByText('ADVANTAGE')).toBeInTheDocument();
   });
